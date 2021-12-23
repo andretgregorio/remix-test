e136a5ae33f9c47675ec1e28dd72ffb15cb5029f
André T Gregório <andretgregorio@gmail.com>
2021-12-23 18:20:13 -0300

    :sparkles: Add header

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,28 +1,9 @@
 import type { LinksFunction } from 'remix';
 import { Link } from 'remix';
-import stylesUrl from '../styles/index.css';
-
-export const links: LinksFunction = () => {
-  return [{ rel: 'stylesheet', href: stylesUrl }];
-};
+import Header from '~/shared/components/header';
 
 function Index() {
-  return (
-    <div className="container">
-      <div className="content">
-        <h1 className="text-yellow-600">
-          Remix <span>Jokes!</span>
-        </h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="jokes">Read Jokes</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  );
+  return <Header />;
 }
 
 export default Index;
---
+++ app/shared/components/header/index.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="w-full bg-black">
+      <div className="container mx-auto">
+        <span className="text-white">Voltz</span>
+      </div>
+    </div>
+  );
+}
